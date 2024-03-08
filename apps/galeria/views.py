2d85088786dc715eafc6e4ab5630859a420e1292
Dragonofdark <lagendarygamer88@gmail.com>
2024-03-08 15:30:47 -0300

    Adicionado um formulario para nova imagem e icone para nova imagem (Precisa ser ajustado)

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
 from django.contrib import messages
+from apps.galeria.forms import FotografiaForm
 
 def index(request):
     if not request.user.is_authenticated:
@@ -27,3 +28,23 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
     return render(request, 'galeria/buscar.html', {'cards': fotografias})
+
+def nova_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Você precisa estar logado para visualizar o site.')
+        return redirect('login')
+    form = FotografiaForm
+    if request.method == 'POST':
+        form = FotografiaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia cadastrada com sucesso!')
+            return redirect('index')
+        
+    return render(request, 'galeria/nova_imagem.html', {'form': form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    pass
